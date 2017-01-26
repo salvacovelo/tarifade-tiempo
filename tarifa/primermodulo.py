eedef87b9593e8704c9968e6898cbbcc5404e1a2
ungluedsalva <ungluedsalva@ungluedsalva-PC>
2017-01-25 20:38:16 -0400

    primer commit

---
+++ tarifa/primermodulo.py
@@ -0,0 +1,32 @@
+'''
+Created on Jan 25, 2017
+
+@author: ungluedsalva
+'''
+from datetime import datetime, date
+import time
+
+
+time1= datetime.strptime("25/1/2017 8:55", "%d/%m/%Y %H:%M")
+time2= datetime.strptime("27/1/2017 8:55", "%d/%m/%Y %H:%M")
+tiempoDeServicio=((time2-time1).total_seconds())
+tarifa=2
+def calcularPrecio(tarifa,tiempoDeServicio):
+    
+    if (tarifa<0):
+        print ("ERROR la tarifa no puede ser menor a 0")
+        exit() 
+        pass
+    else: 
+        if((tiempoDeServicio/60)<15):
+            print(tiempoDeServicio/60)
+
+            print("ERROR no cumple el minimo de tiempo")
+            exit()
+        else:
+            result=((tiempoDeServicio/3600)*tarifa)
+            
+    return result 
+
+precio=calcularPrecio(tarifa,tiempoDeServicio)
+print(precio)
